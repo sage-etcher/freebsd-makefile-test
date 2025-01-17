70f72966f832e0872a448f45a90cb2c6535dc4a1
sage-etcher <sage.code@email.com>
2025-01-17 20:18:48 +0000

    linter+formatting fixes

--- helper.c
+++ helper.c
@@ -2,8 +2,15 @@
 #include "helper.h"
 
 #include <SDL2/SDL.h>
-#include <stddef.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+
+#include <stdint.h> /* NOLINT: Allow indirect header include */
 #include <stdlib.h>
+#include <string.h>
 
 
 typedef struct my_app
@@ -67,24 +74,24 @@ app_alloc (void)
 {
     /* allocate structure */
     my_app_t *app = malloc (sizeof (my_app_t));
-    if (!app) return NULL;
+    if (!app) { return NULL; }
 
     /* default all to null */
     (void)memset (app, 0, sizeof (my_app_t));
-   
+
     return app;
 }
 
 
 int
-app_create_window (my_app_t *app, char *title, int w, int h)
+app_create_window (my_app_t *app, char *title, int width, int height)
 {
     const int win_flags = SDL_WINDOW_RESIZABLE;
     const int rend_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
 
     const int auto_pos = SDL_WINDOWPOS_UNDEFINED;
 
-    app->win = SDL_CreateWindow (title, auto_pos, auto_pos, w, h, win_flags);
+    app->win = SDL_CreateWindow (title, auto_pos, auto_pos, width, height, win_flags);
     app->rend = SDL_CreateRenderer (app->win, 0, rend_flags);
 
     if ((!app->win) || (!app->rend))
@@ -104,20 +111,25 @@ app_destroy_window (my_app_t *app)
     SDL_DestroyWindow (app->win);
 
     app->rend = NULL;
-    app->win  = NULL;
+    app->win = NULL;
 
     return;
 }
 
 
 static SDL_Color
-hex_to_color (uint32_t hex, uint8_t alpha)
+hex_to_color (uint32_t hex, uint8_t alpha) /* NOLINT: Allow adjacent convertible params */
 {
     /* hex to rgba */
-    SDL_Color color = { 0 };
-    color.r = ((hex & 0x00FF0000) >> (2*8));
-    color.g = ((hex & 0x0000FF00) >> (1*8));
-    color.b = ((hex & 0x000000FF) >> (0*8));
+    const int BITS_PER_COLOR = 8;
+    const int R_MASK = 0x00FF0000;
+    const int G_MASK = 0x0000FF00;
+    const int B_MASK = 0x000000FF;
+
+    SDL_Color color = {0};
+    color.r = (hex & R_MASK) >> (2 * BITS_PER_COLOR);
+    color.g = (hex & G_MASK) >> (1 * BITS_PER_COLOR);
+    color.b = (hex & B_MASK) >> (0 * BITS_PER_COLOR);
     color.a = alpha;
 
     return color;
@@ -132,7 +144,8 @@ app_draw_background (my_app_t *app)
     return;
 }
 
-void 
+
+void
 app_present (my_app_t *app)
 {
     SDL_RenderPresent (app->rend);
@@ -151,15 +164,18 @@ app_background_color (my_app_t *app, uint32_t hex, uint8_t alpha)
 void
 app_handle_events (my_app_t *app)
 {
-    SDL_Event e;
+    SDL_Event event;
 
-    while (SDL_PollEvent (&e)) 
+    while (SDL_PollEvent (&event))
     {
-        switch (e.type)
+        switch (event.type)
         {
         case SDL_QUIT:
             app_exit (app);
             return;
+
+        default:
+            break;
         }
     }
 
--- main.c
+++ main.c
@@ -1,25 +1,30 @@
 
-#include <stddef.h>
 #include "helper.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 
-int 
+int
 main (int argc, char **argv)
 {
+    const int BASE_WIDTH  = 640;
+    const int BASE_HEIGHT = 480;
+    const int BASE_BG_HEX = 0xffaabb;
     my_app_t *app = NULL;
 
     app_init ();
 
     app = app_alloc ();
-    app_create_window (app, "my app", 640, 480);
-    app_background_color (app, 0xffaabb, OPAQUE);
+    app_create_window (app, "my app", BASE_WIDTH, BASE_HEIGHT);
+    app_background_color (app, BASE_BG_HEX, OPAQUE);
 
     /* runtime loop */
     while (!app_should_exit (app))
     {
         /* handle events */
         app_handle_events (app);
-        if (app_should_exit (app)) goto main_exit;
+        if (app_should_exit (app)) { goto main_exit; }
 
         /* draw the window */
         app_draw_background (app);
